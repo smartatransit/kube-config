6a9fe0cad9918a3834b26979ba381a34d5f0c8a7
Xander Flood <xander.t.flood@gmail.com>
2020-09-25 20:50:02 -0400

    cleanup the gateway code

--- api-gateway.tf
+++ api-gateway.tf
@@ -4,6 +4,9 @@ resource "kubernetes_namespace" "api-gateway" {
   }
 }
 
+###################################
+### Deploy the API gateway pods ###
+###################################
 resource "kubernetes_deployment" "api-gateway" {
   metadata {
     name      = "api-gateway"
@@ -25,9 +28,6 @@ resource "kubernetes_deployment" "api-gateway" {
       }
 
       spec {
-        service_account_name            = "api-gateway"
-        automount_service_account_token = true
-
         container {
           image = "smartatransit/api-gateway:build-30"
           name  = "api-gateway"
@@ -51,7 +51,59 @@ resource "kubernetes_deployment" "api-gateway" {
 
           port {
             name           = "web"
-            container_port = 80
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+#######################################################################
+### Create Traefik ingress for the direct access to the API gateway ###
+#######################################################################
+resource "kubernetes_service" "api-gateway" {
+  metadata {
+    name      = "api-gateway"
+    namespace = "api-gateway"
+  }
+
+  spec {
+    selector = {
+      app = "traefik"
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
+resource "kubernetes_ingress" "api-gateway" {
+  metadata {
+    name      = "api-gateway"
+    namespace = "api-gateway"
+    annotations = {
+      "kubernetes.io/ingress.class" = "traefik"
+
+      "traefik.ingress.kubernetes.io/router.entrypoints"        = "web-secure"
+      "traefik.ingress.kubernetes.io/router.tls.certresolver"   = "main"
+      "traefik.ingress.kubernetes.io/router.tls.domains.0.main" = "api-gateway.${var.services_domain}"
+
+      # TODO configure SANs for TLS
+      # "traefik.ingress.kubernetes.io/router.tls.domains.0.sans" = "dashboard.${san}"
+    }
+  }
+
+  spec {
+    rule {
+      host = "api-gateway.${var.services_domain}"
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = kubernetes_service.api-gateway.metadata.0.name
+            service_port = 80
           }
         }
       }
